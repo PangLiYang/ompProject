94e33b3e3cc9664065f8e4c025c2cf850d0e3519
pangli <py2236@nyu.edu>
2024-04-10 23:12:09 -0400

    0410

--- JohnsonPar.cpp
+++ JohnsonPar.cpp
@@ -18,7 +18,7 @@ vector<vector<int> > *JohnsonPar::forward(vector<vector<int> > *graph) {
     #pragma omp parallel
     {
 
-        #pragma omp for
+//        #pragma omp for
         for (int i = 0; i < V; i += 1) {
             for (int j = 0; j < V; j += 1) {
                 if (i != j && graph->at(i).at(j) != INT_MAX / 2) {
@@ -56,7 +56,7 @@ vector<vector<int> > *JohnsonPar::forward(vector<vector<int> > *graph) {
             }
         }
 
-        #pragma omp for collapse(2)
+//        #pragma omp for collapse(2)
         for (int i = 0; i < graph_size; i += 1) {
             for (int j = 0; j < graph_size; j += 1) {
                 output->at(i).at(j) = temp->at(i).at(V - graph_size + j);
