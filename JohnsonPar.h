2f9629486007639d4cf56871e6e1a27941b38e20
pangli <py2236@nyu.edu>
2024-04-10 22:47:07 -0400

    0410

--- JohnsonPar.h
+++ JohnsonPar.h
@@ -1,5 +1,5 @@
-#ifndef OMPPROJECT_JOHNSONSEQ_H
-#define OMPPROJECT_JOHNSONSEQ_H
+#ifndef OMPPROJECT_JOHNSONPAR_H
+#define OMPPROJECT_JOHNSONPAR_H
 
 #include <unordered_map>
 #include <list>
@@ -11,18 +11,19 @@ typedef struct graph {
     unordered_map<int, list<pair<int, int> > > adjList;
 } graph_t;
 
-class JohnsonSeq : public Solver {
+class JohnsonPar : public Solver {
 public:
-    JohnsonSeq(int graph_size);
+    JohnsonPar(int num_thread, int graph_size);
 
     vector< vector<int> >* forward(vector< vector<int> >* graph);
     vector< vector<int> >* forward_optimized(vector< vector<int> >* graph);
 
 private:
     int graph_size;
+    int num_thread;
 
     graph_t* init_adjacency_list(vector< vector<int> >* graph);
 };
 
 
-#endif //OMPPROJECT_JOHNSONSEQ_H
+#endif //OMPPROJECT_JOHNSONPAR_H
