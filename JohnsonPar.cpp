8a4e1236444a8b4018f85fec7ae078efe9cb5586
pangli <py2236@nyu.edu>
2024-04-10 23:40:08 -0400

    0410

--- JohnsonPar.cpp
+++ JohnsonPar.cpp
@@ -14,8 +14,6 @@ vector<vector<int> > *JohnsonPar::forward(vector<vector<int> > *graph) {
 
     auto *temp = new vector<vector<int> >(V, vector<int>(V));
     auto *output = new vector<vector<int> >(graph_size, vector<int>(graph_size));
-    vector<int> curr(V, INT_MAX / 2);
-    priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq;
 
     for (int i = 0; i < V; i += 1) {
 
@@ -26,11 +24,11 @@ vector<vector<int> > *JohnsonPar::forward(vector<vector<int> > *graph) {
         }
     }
 
-    #pragma omp parallel for private(curr, pq)
+    #pragma omp parallel for
     for (int u = 0; u < V; u += 1) {
 
-//        vector<int> curr(V, INT_MAX / 2);
-//        priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq;
+        vector<int> curr(V, INT_MAX / 2);
+        priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq;
 
         curr.at(u) = 0;
         pq.push(make_pair(0, u));
@@ -55,6 +53,7 @@ vector<vector<int> > *JohnsonPar::forward(vector<vector<int> > *graph) {
         }
     }
 
+    #pragma omp parallel for collapse(2)
     for (int i = 0; i < graph_size; i += 1) {
         for (int j = 0; j < graph_size; j += 1) {
             output->at(i).at(j) = temp->at(i).at(V - graph_size + j);
