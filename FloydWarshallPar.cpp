59c9e848d51f12a6d3ae9fe51e4adddcee3f9cdd
pangli <py2236@nyu.edu>
2024-04-16 00:25:34 -0400

    0416

--- FloydWarshallPar.cpp
+++ FloydWarshallPar.cpp
@@ -23,7 +23,7 @@ vector< vector<int> >* FloydWarshallPar::forward(vector< vector<int> >* graph) {
         }
 
         for (int k = 0; k < n; k += 1) {
-            #pragma omp for collapse(2)
+            #pragma omp for
             for (int i = 0; i < n; i += 1) {
                 for (int j = 0; j < n; j += 1) {
                     if (local_graph->at(i).at(j) > local_graph->at(i).at(k) + local_graph->at(k).at(j)) {
